2d44201315f44c3bc7fb23db1eaa152bc1b7e6d7
Doğukan Öztürk <125322507+hackInForeach@users.noreply.github.com>
2024-02-01 13:38:27 +0300

    Add files via upload

---
+++ SinirBozucuRadioButton/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace SinirBozucuRadioButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbtn1 = new System.Windows.Forms.RadioButton();
+            this.rbtn2 = new System.Windows.Forms.RadioButton();
+            this.rbtn3 = new System.Windows.Forms.RadioButton();
+            this.rbtn4 = new System.Windows.Forms.RadioButton();
+            this.rbtn5 = new System.Windows.Forms.RadioButton();
+            this.rbtn6 = new System.Windows.Forms.RadioButton();
+            this.rbtn7 = new System.Windows.Forms.RadioButton();
+            this.rbtn8 = new System.Windows.Forms.RadioButton();
+            this.rbtn9 = new System.Windows.Forms.RadioButton();
+            this.rbtn10 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(69, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(429, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lütfen Aşağıdaki Seçeneklerden Birini Seçin:";
+            // 
+            // rbtn1
+            // 
+            this.rbtn1.AutoSize = true;
+            this.rbtn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtn1.Location = new System.Drawing.Point(34, 96);
+            this.rbtn1.Name = "rbtn1";
+            this.rbtn1.Size = new System.Drawing.Size(152, 24);
+            this.rbtn1.TabIndex = 1;
+            this.rbtn1.Text = "Çok oyun oynarım";
+            this.rbtn1.UseVisualStyleBackColor = true;
+            // 
+            // rbtn2
+            // 
+            this.rbtn2.AutoSize = true;
+            this.rbtn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtn2.Location = new System.Drawing.Point(34, 119);
+            this.rbtn2.Name = "rbtn2";
+            this.rbtn2.Size = new System.Drawing.Size(161, 24);
+            this.rbtn2.TabIndex = 2;
+            this.rbtn2.Text = "Hiç oyun oynamam";
+            this.rbtn2.UseVisualStyleBackColor = true;
+            // 
+            // rbtn3
+            // 
+            this.rbtn3.AutoSize = true;
+            this.rbtn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtn3.Location = new System.Drawing.Point(34, 142);
+            this.rbtn3.Name = "rbtn3";
+            this.rbtn3.Size = new System.Drawing.Size(148, 24);
+            this.rbtn3.TabIndex = 3;
+            this.rbtn3.Text = "Televizyon izlerim";
+            this.rbtn3.UseVisualStyleBackColor = true;
+            // 
+            // rbtn4
+            // 
+            this.rbtn4.AutoSize = true;
+            this.rbtn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtn4.Location = new System.Drawing.Point(34, 165);
+            this.rbtn4.Name = "rbtn4";
+            this.rbtn4.Size = new System.Drawing.Size(223, 24);
+            this.rbtn4.TabIndex = 4;
+            this.rbtn4.Text = "Canım sıkılıkça spor yaparım";
+            this.rbtn4.UseVisualStyleBackColor = true;
+            // 
+            // rbtn5
+            // 
+            this.rbtn5.AutoSize = true;
+            this.rbtn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtn5.Location = new System.Drawing.Point(34, 188);
+            this.rbtn5.Name = "rbtn5";
+            this.rbtn5.Size = new System.Drawing.Size(114, 24);
+            this.rbtn5.TabIndex = 5;
+            this.rbtn5.Text = "Ben ve ötesi";
+            this.rbtn5.UseVisualStyleBackColor = true;
+            // 
+            // rbtn6
+            // 
+            this.rbtn6.AutoSize = true;
+            this.rbtn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtn6.Location = new System.Drawing.Point(289, 96);
+            this.rbtn6.Name = "rbtn6";
+            this.rbtn6.Size = new System.Drawing.Size(238, 24);
+            this.rbtn6.TabIndex = 6;
+            this.rbtn6.Text = "Ben hiç birşeyden hoşlanmam";
+            this.rbtn6.UseVisualStyleBackColor = true;
+            // 
+            // rbtn7
+            // 
+            this.rbtn7.AutoSize = true;
+            this.rbtn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtn7.Location = new System.Drawing.Point(289, 119);
+            this.rbtn7.Name = "rbtn7";
+            this.rbtn7.Size = new System.Drawing.Size(168, 24);
+            this.rbtn7.TabIndex = 7;
+            this.rbtn7.Text = "Ben herşeyi severim";
+            this.rbtn7.UseVisualStyleBackColor = true;
+            // 
+            // rbtn8
+            // 
+            this.rbtn8.AutoSize = true;
+            this.rbtn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtn8.Location = new System.Drawing.Point(289, 142);
+            this.rbtn8.Name = "rbtn8";
+            this.rbtn8.Size = new System.Drawing.Size(205, 24);
+            this.rbtn8.TabIndex = 8;
+            this.rbtn8.Text = "Bunlan bence çok saçma";
+            this.rbtn8.UseVisualStyleBackColor = true;
+            // 
+            // rbtn9
+            // 
+            this.rbtn9.AutoSize = true;
+            this.rbtn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtn9.Location = new System.Drawing.Point(289, 165);
+            this.rbtn9.Name = "rbtn9";
+            this.rbtn9.Size = new System.Drawing.Size(255, 24);
+            this.rbtn9.TabIndex = 9;
+            this.rbtn9.Text = "Şuan tercih yapmak istemiyorum";
+            this.rbtn9.UseVisualStyleBackColor = true;
+            // 
+            // rbtn10
+            // 
+            this.rbtn10.AutoSize = true;
+            this.rbtn10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbtn10.Location = new System.Drawing.Point(289, 188);
+            this.rbtn10.Name = "rbtn10";
+            this.rbtn10.Size = new System.Drawing.Size(113, 24);
+            this.rbtn10.TabIndex = 10;
+            this.rbtn10.Text = "Belki bir gün";
+            this.rbtn10.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(197, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 39);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Onayla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 305);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rbtn1);
+            this.Controls.Add(this.rbtn10);
+            this.Controls.Add(this.rbtn2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rbtn9);
+            this.Controls.Add(this.rbtn6);
+            this.Controls.Add(this.rbtn3);
+            this.Controls.Add(this.rbtn5);
+            this.Controls.Add(this.rbtn8);
+            this.Controls.Add(this.rbtn7);
+            this.Controls.Add(this.rbtn4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbtn1;
+        private System.Windows.Forms.RadioButton rbtn2;
+        private System.Windows.Forms.RadioButton rbtn3;
+        private System.Windows.Forms.RadioButton rbtn4;
+        private System.Windows.Forms.RadioButton rbtn5;
+        private System.Windows.Forms.RadioButton rbtn6;
+        private System.Windows.Forms.RadioButton rbtn7;
+        private System.Windows.Forms.RadioButton rbtn8;
+        private System.Windows.Forms.RadioButton rbtn9;
+        private System.Windows.Forms.RadioButton rbtn10;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SinirBozucuRadioButton/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SinirBozucuRadioButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            int tut = rand.Next(1,11);
+
+            if (tut == 1)
+                rbtn9.Checked = true;
+            if (tut == 2)
+                rbtn10.Checked = true;
+            if (tut == 3)
+                rbtn7.Checked = true;
+            if (tut == 4)
+                rbtn8.Checked = true;
+            if (tut == 5)
+                rbtn6.Checked = true;
+            if (tut == 6)
+                rbtn5.Checked = true;
+            if (tut == 7)
+                rbtn3.Checked = true;
+            if (tut == 8)
+                rbtn4.Checked = true;
+            if (tut == 9)
+                rbtn1.Checked = true;
+            if (tut == 10)
+                rbtn2.Checked = true;
+
+        }
+    }
+}
