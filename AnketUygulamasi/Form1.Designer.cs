303d41b477650f5686487745824e4420766ae087
Doğukan Öztürk <125322507+hackInForeach@users.noreply.github.com>
2024-02-01 13:36:28 +0300

    Add files via upload

---
+++ AnketUygulamasi/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace AnketUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxCinsiyet = new System.Windows.Forms.ComboBox();
+            this.cbxYas = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxSpor = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbxMuzik = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbxFilm = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbxHayvan = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbxKitap = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbxTatli = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnOnay = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbxIsim = new System.Windows.Forms.TextBox();
+            this.lblCikti = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(9, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hangi cinsiyete aitsiniz?";
+            // 
+            // cbxCinsiyet
+            // 
+            this.cbxCinsiyet.FormattingEnabled = true;
+            this.cbxCinsiyet.Location = new System.Drawing.Point(12, 184);
+            this.cbxCinsiyet.Name = "cbxCinsiyet";
+            this.cbxCinsiyet.Size = new System.Drawing.Size(157, 21);
+            this.cbxCinsiyet.TabIndex = 1;
+            // 
+            // cbxYas
+            // 
+            this.cbxYas.FormattingEnabled = true;
+            this.cbxYas.Location = new System.Drawing.Point(12, 265);
+            this.cbxYas.Name = "cbxYas";
+            this.cbxYas.Size = new System.Drawing.Size(157, 21);
+            this.cbxYas.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(9, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Yaşınız nedir?";
+            // 
+            // cbxSpor
+            // 
+            this.cbxSpor.FormattingEnabled = true;
+            this.cbxSpor.Location = new System.Drawing.Point(12, 337);
+            this.cbxSpor.Name = "cbxSpor";
+            this.cbxSpor.Size = new System.Drawing.Size(157, 21);
+            this.cbxSpor.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(9, 309);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hangi sporu tercih edersiniz?";
+            // 
+            // cbxMuzik
+            // 
+            this.cbxMuzik.FormattingEnabled = true;
+            this.cbxMuzik.Location = new System.Drawing.Point(12, 413);
+            this.cbxMuzik.Name = "cbxMuzik";
+            this.cbxMuzik.Size = new System.Drawing.Size(157, 21);
+            this.cbxMuzik.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(9, 385);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(380, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Hangi müzik türünü dinlemeyi tercih edersiniz?";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // cbxFilm
+            // 
+            this.cbxFilm.FormattingEnabled = true;
+            this.cbxFilm.Location = new System.Drawing.Point(451, 184);
+            this.cbxFilm.Name = "cbxFilm";
+            this.cbxFilm.Size = new System.Drawing.Size(157, 21);
+            this.cbxFilm.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(448, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(240, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Hangi film türünü seversiniz?";
+            // 
+            // cbxHayvan
+            // 
+            this.cbxHayvan.FormattingEnabled = true;
+            this.cbxHayvan.Location = new System.Drawing.Point(451, 413);
+            this.cbxHayvan.Name = "cbxHayvan";
+            this.cbxHayvan.Size = new System.Drawing.Size(157, 21);
+            this.cbxHayvan.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(448, 385);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(293, 20);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Hangi hayvanı daha çok seversiniz?";
+            // 
+            // cbxKitap
+            // 
+            this.cbxKitap.FormattingEnabled = true;
+            this.cbxKitap.Location = new System.Drawing.Point(451, 341);
+            this.cbxKitap.Name = "cbxKitap";
+            this.cbxKitap.Size = new System.Drawing.Size(157, 21);
+            this.cbxKitap.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(448, 313);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(364, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Hangi kitap türünü okumayı tercih edersiniz?";
+            // 
+            // cbxTatli
+            // 
+            this.cbxTatli.FormattingEnabled = true;
+            this.cbxTatli.Location = new System.Drawing.Point(451, 265);
+            this.cbxTatli.Name = "cbxTatli";
+            this.cbxTatli.Size = new System.Drawing.Size(157, 21);
+            this.cbxTatli.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(448, 237);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(240, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Hangi tatlıyı tercih edersiniz?";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(213, 40);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(327, 25);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Aşağıdaki soruları yanıtlayınız";
+            // 
+            // btnOnay
+            // 
+            this.btnOnay.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnOnay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOnay.Location = new System.Drawing.Point(452, 457);
+            this.btnOnay.Name = "btnOnay";
+            this.btnOnay.Size = new System.Drawing.Size(119, 49);
+            this.btnOnay.TabIndex = 21;
+            this.btnOnay.Text = "Tercihlerimi Onayla";
+            this.btnOnay.UseVisualStyleBackColor = true;
+            this.btnOnay.Click += new System.EventHandler(this.btnOnay_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(12, 108);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(122, 20);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "İsminiz Nedir?";
+            // 
+            // tbxIsim
+            // 
+            this.tbxIsim.Location = new System.Drawing.Point(152, 110);
+            this.tbxIsim.Name = "tbxIsim";
+            this.tbxIsim.Size = new System.Drawing.Size(186, 20);
+            this.tbxIsim.TabIndex = 23;
+            // 
+            // lblCikti
+            // 
+            this.lblCikti.AutoSize = true;
+            this.lblCikti.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCikti.Location = new System.Drawing.Point(66, 457);
+            this.lblCikti.Name = "lblCikti";
+            this.lblCikti.Size = new System.Drawing.Size(0, 18);
+            this.lblCikti.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 692);
+            this.Controls.Add(this.lblCikti);
+            this.Controls.Add(this.tbxIsim);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btnOnay);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cbxHayvan);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbxKitap);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbxTatli);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cbxFilm);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbxMuzik);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbxSpor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbxYas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxCinsiyet);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Anket";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxCinsiyet;
+        private System.Windows.Forms.ComboBox cbxYas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxSpor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbxMuzik;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbxFilm;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbxHayvan;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbxKitap;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbxTatli;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnOnay;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbxIsim;
+        private System.Windows.Forms.Label lblCikti;
+    }
+}
+
---
+++ AnketUygulamasi/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnketUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            cbxCinsiyet.Items.Add("Erkek");
+            cbxCinsiyet.Items.Add("Kadın");
+            cbxCinsiyet.Items.Add("Diğer");
+            cbxCinsiyet.Items.Add("Belirtmek istemiyorum");
+
+            cbxYas.Items.Add("18'den küçük");
+            cbxYas.Items.Add("18 - 24");
+            cbxYas.Items.Add("25 - 44");
+            cbxYas.Items.Add("45 ve üzeri");
+
+            cbxSpor.Items.Add("Futol");
+            cbxSpor.Items.Add("Basketbol");
+            cbxSpor.Items.Add("Voleybol");
+            cbxSpor.Items.Add("Tenis");
+
+            cbxMuzik.Items.Add("Pop");
+            cbxMuzik.Items.Add("Rock");
+            cbxMuzik.Items.Add("Klasik");
+            cbxMuzik.Items.Add("Hip - Hop");
+
+            cbxFilm.Items.Add("Aksiyon");
+            cbxFilm.Items.Add("Komedi");
+            cbxFilm.Items.Add("Romantik");
+            cbxFilm.Items.Add("Belgesel");
+
+            cbxTatli.Items.Add("Çikolata");
+            cbxTatli.Items.Add("Dondurma");
+            cbxTatli.Items.Add("Kek");
+            cbxTatli.Items.Add("Muffin");
+
+            cbxKitap.Items.Add("Polisiye");
+            cbxKitap.Items.Add("Bilimkurgu");
+            cbxKitap.Items.Add("Klasik Edebiyat");
+            cbxKitap.Items.Add("Gizem");
+
+            cbxHayvan.Items.Add("Kaplumbağa");
+            cbxHayvan.Items.Add("Kedi");
+            cbxHayvan.Items.Add("Tavşan");
+            cbxHayvan.Items.Add("Kuş");
+
+
+        }
+
+        private void btnOnay_Click(object sender, EventArgs e)
+        {
+            int cinsiyet=cbxCinsiyet.SelectedIndex;
+            int yas=cbxYas.SelectedIndex;
+            int spor=cbxSpor.SelectedIndex;
+            int muzik=cbxMuzik.SelectedIndex;
+            int film=cbxFilm.SelectedIndex;
+            int tatli=cbxTatli.SelectedIndex;
+            int kitap=cbxKitap.SelectedIndex;
+            int hayvan=cbxHayvan.SelectedIndex;
+
+            if (string.IsNullOrWhiteSpace(tbxIsim.Text))
+            {
+                MessageBox.Show("İsim boş brakılamaz!!...");
+            }
+            else
+            {
+                if (cinsiyet < 0 | yas < 0 | spor < 0 | muzik < 0 | film < 0 | tatli < 0 | kitap < 0 | hayvan < 0)
+                   MessageBox.Show("Her alanı seç!!");
+                    
+                else
+                {
+
+                    // lblCikti.Text =
+                    MessageBox.Show(
+                                    "Adı:  " + tbxIsim.Text + "\n" +
+                                    "Cinsiyeti:  " + cbxCinsiyet.Text + "\n" +
+                                    "Yaş Aralığı:  " + cbxYas.Text + "\n" +
+                                    "En Sevdiği Spor:  " + cbxSpor.Text + "\n" +
+                                    "En Sevdiği Müzik:  " + cbxMuzik.Text + "\n" +
+                                    "En Sevdiği Film:  " + cbxFilm.Text + "\n" +
+                                    "En Sevdiği Tatlı:  " + cbxTatli.Text + "\n" +
+                                    "En Sevdiği Kitap:  " + cbxKitap.Text + "\n" +
+                                    "En Sevdiği Hayvan:  " + cbxHayvan.Text
+                                    );
+                }
+            }
+            tbxIsim.Text = "";
+            cbxCinsiyet.Text = "";
+            cbxYas.Text = "";
+            cbxSpor.Text = "";
+            cbxMuzik.Text = "";
+            cbxFilm.Text = "";
+            cbxTatli.Text = "";
+            cbxKitap.Text = "";
+            cbxHayvan.Text = "";
+
+        }
+    }
+}
